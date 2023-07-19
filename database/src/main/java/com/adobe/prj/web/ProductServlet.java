450cbe6a6bec090abee6f9dc2e3e4d846c77d8b9
Banuprakash <banuprakashc@yahoo.co.in>
2023-07-19 16:01:23 +0530

    day 3

--- database/src/main/java/com/adobe/prj/web/ProductServlet.java
+++ database/src/main/java/com/adobe/prj/web/ProductServlet.java
@@ -4,22 +4,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.adobe.prj.dao.FetchException;
 import com.adobe.prj.dao.ProductDao;
 import com.adobe.prj.dao.ProductDaoJdbcImpl;
 import com.adobe.prj.entity.Product;
 
-@WebServlet("/products")
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(value="products")
 public class ProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter out = response.getWriter(); // character stream to browser / client
 		response.setContentType("text/html"); // MIME type
